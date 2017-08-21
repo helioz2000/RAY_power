95ea0d533530bd89ee78dd206182c0591883d046
helioz2000 <helioz2000@gmail.com>
2017-08-21 20:35:41 +1000

    Move Strings to Flash
    Strings stored in Flash
    Voltages now stored as byte

--- RAY_power.ino
+++ RAY_power.ino
@@ -44,10 +44,10 @@ enum pwrStateType {
 };
 
 typedef struct {
-  char description[20];
+  char description[11];
   byte pin;
-  float offVoltage;
-  float onVoltage;
+  byte offVoltage;    // implied decimal point (e.g. 125 = 12.5V)
+  byte onVoltage;     // implied decimal point
   uint16_t onDelay;     // seconds required above onVoltage before switching On
   uint16_t offDelay;    // seconds below offVoltage before switching off
   byte currentState;
@@ -56,12 +56,12 @@ typedef struct {
 } device_item;
 
 device_item pwr_device[DEVICE_COUNT] = {
-  {"2mt Repeater", 9, 10.5, 13.0, 180, 10, sOn, 0, 0},
-  {"70cm Repeater", 8, 11.5, 13.0, 300, 10, sOn, 0, 0},
-  {"Spare CH3", 7, 15.0, 16.0, 300, 10, sOff, 0, 0}, 
-  {"Spare CH4", 6, 15.0, 16.0, 300, 10, sOff, 0, 0},
-  {"Telemetry", 5, 11.5, 13.0, 300, 10, sOn, 0, 0}
-//  {"Relay 6", 4, 11.5, 13.0, 300, 10, sOn, 0, 0}
+  {"2mt Rptr ", 9, 105, 130, 180, 10, sOn, 0, 0},
+  {"70cm Rptr", 8, 115, 130, 300, 10, sOn, 0, 0},
+  {"Spare CH3", 7, 150, 160, 300, 10, sOff, 0, 0}, 
+  {"Spare CH4", 6, 150, 160, 300, 10, sOff, 0, 0},
+  {"Telemetry", 5, 115, 130, 300, 10, sOn, 0, 0}
+//  {"Relay 6", 4, 115, 13.0, 300, 10, sOn, 0, 0}
 };
 
 #define PWR_ON true     // Output state for device ON
@@ -84,11 +84,13 @@ SoftwareSerial tty1(T1_RX_PIN, T1_TX_PIN); // RX, TX
 
 static bool tty0_connected = false;
 static bool tty1_connected = false;
-
+uint16_t rx_parameter;         // to store a parameter > 1 byte received from tty
 unsigned long tty0_disconnect_time;
 unsigned long tty1_disconnect_time;
 
-bool tty0_authorized = false;
+bool tty0_authorized = true;  // !! should be false;
+
+static byte term_config_device = 0;   // device config mode when > 0
 
 #define TERMINAL_TIMEOUT 180    // seconds
 
@@ -105,7 +107,8 @@ const char password[] = "20[awarc]17";
 
 bool overrideMode=false;    // manual control of power circuits via terminal
 
-static float batteryV;
+//static float batteryV;
+static byte batV;   // battery voltage as byte with implied decimal point
 static float scaleFactor = ANALOG_REFERENCE_V / ANALOG_REFERENCE_RAW;
 
 debugLevels currentDebugLevel = L_INFO;
@@ -115,17 +118,17 @@ void setup() {
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
 
+  // Start terminal interfaces
+  tty0.begin(TERMINAL_BAUD);
+  tty0.println(F("Started tt0"));
+  tty1.begin(TERMINAL_BAUD);
+  tty1.println(F("Started tt1"));
+
   // configure pwr device outputs and switch them the pre-set state
   for (int i=0; i < DEVICE_COUNT; i++) {
     pinMode(pwr_device[i].pin, OUTPUT);
     devicePwr(i+1, pwr_device[i].currentState);
   }
-
-  // Start terminal interfaces
-  tty0.begin(TERMINAL_BAUD);
-  tty0.println("Started tt0");
-  tty1.begin(TERMINAL_BAUD);
-  tty1.println("Started tt1");
 }
 
 void loop() {
@@ -134,70 +137,6 @@ void loop() {
   if (!overrideMode) {
     processDevices();
   }
-}
-
-void processDevices () {
-  for (int i=0; i<DEVICE_COUNT; i++) {   
-     
-    // is the device ON?
-    if (pwr_device[i].currentState == sOn) {
-      // is battery voltage above cutout?
-      if (batteryV > pwr_device[i].offVoltage) {
-        continue; // nothing to do, process next device
-      } else {  // battery voltage is below cutout?
-        // start the off delay
-        pwr_device[i].currentState = sOffDelay;
-        pwr_device[i].offTime = millis() + ((unsigned long)pwr_device[i].offDelay * (unsigned long)1000);
-      }
-    }
-
-    // is the device OFF?
-    if (pwr_device[i].currentState == sOff) {
-      // is battery below on voltage?
-      if (batteryV < pwr_device[i].onVoltage) {
-        continue; // nothing to do, process next device
-      } else {  // battery is above ON voltage
-        // start to on delay
-        pwr_device[i].currentState = sOnDelay;
-        pwr_device[i].onTime = millis() + ((unsigned long)pwr_device[i].onDelay * (unsigned long)1000);
-      }
-    }
-
-    // is the device in OFF delay mode?
-    if (pwr_device[i].currentState == sOffDelay) {
-      // has the voltage risen back up?
-      if (batteryV > pwr_device[i].offVoltage) {
-        // return back to ON status
-        pwr_device[i].currentState = sOn;
-      } else {  //voltage is still below cutout
-        // has the off delay expired?
-        if (millis() >= pwr_device[i].offTime) {
-          devicePwr(i+1, sOff);    // switch power off
-        }
-      }
-    }
-
-    // is the device in ON delay mode?
-    if (pwr_device[i].currentState == sOnDelay) {
-      // has the voltage dropped back down?
-      if (batteryV < pwr_device[i].onVoltage) {
-        // return back to OFF state
-        pwr_device[i].currentState = sOff;
-      } else {    // battery is above on voltage
-        if (millis() >= pwr_device[i].onTime) {
-          devicePwr(i+1, sOn);    // switch power on
-        }
-      }
-    }    
-  } // for loop - device
-}
-
-void readVoltages () {
-  // Batter Voltage
-  // read raw and convert to voltage at AI pin voltage
-  float pinV = (float)analogRead(A0) * scaleFactor;
-  // Use pre-scale to calculate measured voltages (i.e. voltage divider)
-  batteryV = pinV * ANALOG_PRESCALE_A0;
 }
 
 void devicePwr(byte device, byte newPwrState) {
@@ -212,13 +151,81 @@ void devicePwr(byte device, byte newPwrState) {
   // switch ON or OFF
   if (newPwrState == sOn) {
     digitalWrite(pwr_device[device].pin, PWR_ON);
-    terminal_print("%s ON\n\r", pwr_device[device].description);
+    terminal_print(F("%s ON\n\r"), pwr_device[device].description);
   }
   if (newPwrState == sOff) {
     digitalWrite(pwr_device[device].pin, PWR_OFF);
-    terminal_print("%s OFF\n\r", pwr_device[device].description);
+    terminal_print(F("%s OFF\n\r"), pwr_device[device].description);
   }
 }
+
+void processDevices () {
+  for (int i=0; i<DEVICE_COUNT; i++) {   
+     
+    // is the device ON?
+    if (pwr_device[i].currentState == sOn) {
+      // is battery voltage above cutout?
+      if (batV > pwr_device[i].offVoltage) {
+        continue; // nothing to do, process next device
+      } else {  // battery voltage is below cutout?
+        // start the off delay
+        pwr_device[i].currentState = sOffDelay;
+        pwr_device[i].offTime = millis() + ((unsigned long)pwr_device[i].offDelay * (unsigned long)1000);
+      }
+    }
+
+    // is the device OFF?
+    if (pwr_device[i].currentState == sOff) {
+      // is battery below on voltage?
+      if (batV < pwr_device[i].onVoltage) {
+        continue; // nothing to do, process next device
+      } else {  // battery is above ON voltage
+        // start to on delay
+        pwr_device[i].currentState = sOnDelay;
+        pwr_device[i].onTime = millis() + ((unsigned long)pwr_device[i].onDelay * (unsigned long)1000);
+      }
+    }
+
+    // is the device in OFF delay mode?
+    if (pwr_device[i].currentState == sOffDelay) {
+      // has the voltage risen back up?
+      if (batV > pwr_device[i].offVoltage) {
+        // return back to ON status
+        pwr_device[i].currentState = sOn;
+      } else {  //voltage is still below cutout
+        // has the off delay expired?
+        if (millis() >= pwr_device[i].offTime) {
+          devicePwr(i+1, sOff);    // switch power off
+        }
+      }
+    }
+
+    // is the device in ON delay mode?
+    if (pwr_device[i].currentState == sOnDelay) {
+      // has the voltage dropped back down?
+      if (batV < pwr_device[i].onVoltage) {
+        // return back to OFF state
+        pwr_device[i].currentState = sOff;
+      } else {    // battery is above on voltage
+        if (millis() >= pwr_device[i].onTime) {
+          devicePwr(i+1, sOn);    // switch power on
+        }
+      }
+    }    
+  } // for loop - device
+}
+
+/*
+ * Battery Voltage - read raw and convert to voltage at AI pin voltage
+ */
+  
+void readVoltages () {
+  float pinV = (float)analogRead(A0) * scaleFactor;
+  // Use pre-scale to calculate measured voltages (i.e. voltage divider)
+  float batteryV = pinV * ANALOG_PRESCALE_A0;
+  batV = (int)(batteryV *10);   // convert to byte with implied decimal point
+}
+
 
 /*
  * ===============================================================
@@ -250,62 +257,96 @@ void debug(debugLevels level, char *sFmt, ...)  // %f is not supported
 void terminal_print_state(byte device) {
   switch (pwr_device[device].currentState) {
     case sOn:
-      terminal_print("On");
+      terminal_print(F("On"));
       break;
     case sOff:
-      terminal_print("Off");
+      terminal_print(F("Off"));
       break;
     case sOnDelay:
-      terminal_print("On Delay");
+      terminal_print(F("On Delay (On in %lds)"), (pwr_device[device].onTime - millis()) / 1000 );
       break;
     case sOffDelay:
-      terminal_print("Off Delay");
+      terminal_print(F("Off Delay (Off in %lds)"), (pwr_device[device].offTime - millis()) / 1000 );
       break;
     default:
-      terminal_print("Undefined");
+      terminal_print(F("Undefined"));
       break;
   }
 }
 
+/*
+ * print device status for all devices
+ */
 void terminal_status() {
-  terminal_print("\n\rDevice Status:\n\r");
+  terminal_print(F("\n\rDevice Status:\n\r"));
   for (int i=0; i < DEVICE_COUNT; i++) {
-    terminal_print(pwr_device[i].description);
-    terminal_print(" is ");
+    terminal_print(F("%d: %s is"), i+1, pwr_device[i].description);
     terminal_print_state(i);
-    terminal_print("\n\r");
+    terminal_print(F("\n\r"));
   }
-  terminal_print("\n\r");
+  terminal_print(F("\n\r"));
 }
 
+/*
+ * print supply voltage
+ */
 void terminal_voltage() {
-  terminal_print("\n\r============================\n\r");
-  terminal_print("Battery Voltage: %d.%dV\n\r", (int) batteryV, (int) (batteryV *100) % 100);  
+  terminal_print(F("\n\r============================\n\r"));
+  //terminal_print("Battery Voltage: %d.%dV (%d)\n\r", (int) batteryV, (int) (batteryV *100) % 100, batV );  
+  terminal_print(F("Battery Voltage: %d\n\r"), batV );
+}
+
+/*
+ * print device configuration menu
+ */
+void terminal_device_config_menu() {
+  terminal_print(F("Device configuration menu\n\n\r"));
+  // is a valid device selected ?
+  if (term_config_device > DEVICE_COUNT) {
+    terminal_print(F("\n\r [1..%d] Select Device\n\r"), DEVICE_COUNT);
+  } else {
+    terminal_print(F("Selected Device: %d - %s:\n\r"), term_config_device, pwr_device[term_config_device-1].description);
+    terminal_print(F("ON  Voltage: %d   Delay: %d\n\r"), pwr_device[term_config_device-1].onVoltage, pwr_device[term_config_device-1].onDelay);
+    terminal_print(F("OFF Voltage: %d   Delay: %d\n\r"), pwr_device[term_config_device-1].offVoltage, pwr_device[term_config_device-1].offDelay);
+    terminal_print(F("\n\r  nxxx  - set new ON Voltage [Volts * 10]\n\r"));
+    terminal_print(F("  fxxx  - set new OFF Voltage [Volts * 10]\n\r"));
+    terminal_print(F("  txxx  - set new ON Delay [seconds]\n\r"));
+    terminal_print(F("  uxxx  - set new OFF Delay [seconds]\n\r"));
+  }
+  terminal_print(F("  x  - exit device configuration\n\r"));
+  terminal_print(F("\n\r"));
 }
 
 void terminal_menu() {
-  terminal_print("\n\rPower Management V%d.%d\n\r", VERSION_MAJOR, VERSION_MINOR);
+  terminal_print(F("\n\rPower Management V%d.%d\n\r"), VERSION_MAJOR, VERSION_MINOR);
   if (overrideMode) {
-    terminal_print("!!! --- Override Mode Active - Battery Monitor inactive --- !!!\n\r");
+    terminal_print(F("!!! --- Override Mode Active - Battery Monitor inactive --- !!!\n\r"));
   }
-  terminal_print("\n\r  fx - power off, x=1..%d (device number)\n\r", DEVICE_COUNT);
-  terminal_print("  nx - switch on, x=1..%d (device number)\n\r", DEVICE_COUNT);
+
+  if (term_config_device > 0) {
+    terminal_device_config_menu();
+    return;
+    }
+  
+  terminal_print(F("\n\r  fx - power off, x=1..%d (device number)\n\r"), DEVICE_COUNT);
+  terminal_print(F("  nx - switch on, x=1..%d (device number)\n\r"), DEVICE_COUNT);
   if (overrideMode) {
-    terminal_print("  o -  toggle override mode - NOW ACTIVE !\n\r");
+    terminal_print(F("  o -  toggle override mode - NOW ACTIVE !\n\r"));
   } else {
-    terminal_print("  o -  toggle override mode (manual power control)\n\r");
+    terminal_print(F("  o -  toggle override mode (manual power control)\n\r"));
   }
-  terminal_print("  s  - status\n\r");
-  terminal_print("  v  - battery voltage\n\r");
-  terminal_print("  x  - exit (close connection)\n\r");
+  terminal_print(F("  s  - status\n\r"));
+  terminal_print(F("  v  - battery voltage\n\r"));
+  terminal_print(F("  d  - device config\n\r"));
+  terminal_print(F("  x  - exit (close connection)\n\r"));
   if (overrideMode) {
-    terminal_print("\n\r!!! --- Override Mode Active - Battery Monitor inactive --- !!!\n\r");
+    terminal_print(F("\n\r!!! --- Override Mode Active - Battery Monitor inactive --- !!!\n\r"));
   }
-  terminal_print("\n\rEnter command:\n\r");
+  terminal_print(F("\n\rEnter command:\n\r"));
 }
 
 void terminal_invalid() {
-  terminal_print("\n\rInvalid Command\n\r");
+  terminal_print(F("\n\rInvalid Command\n\r"));
 }
 
 byte terminal_verify_device(byte asciiDevNum, bool printInvalid) {
@@ -314,15 +355,49 @@ byte terminal_verify_device(byte asciiDevNum, bool printInvalid) {
   // check if device number is in range
   if ( (asciiDevNum > ('0'+DEVICE_COUNT)) || (asciiDevNum < '1') ) {
     if (printInvalid) {
-      terminal_print("\n\rInvalid device number %c (range is 1 to %d)\n\r", asciiDevNum, DEVICE_COUNT);
+      terminal_print(F("\n\rInvalid device number %c (range is 1 to %d)\n\r"), asciiDevNum, DEVICE_COUNT);
     }
     return (0);
   }
   return (asciiDevNum - 48);
 }
 
-void terminal_process_cmd(byte cmd, byte parameter) {
+void terminal_device_config() {
+  term_config_device = 99;
+}
+
+void terminal_process_device_config_cmd(byte cmd, uint16_t parameter) {
   byte devNum;
+  switch(cmd) {
+    case 'n':   // ON Voltage
+    case 'f':   // OFF Voltage
+      devNum = terminal_verify_device(parameter, true) ;
+      if (devNum)
+        devicePwr(devNum, sOff);
+      break;
+    case 't': // ON Delay
+      break;
+    case 'u': // OFF Delay
+      break;
+    case 'x':
+      term_config_device = 0;
+    default:
+      // did we select a new device number
+      devNum = terminal_verify_device(cmd, true);
+      if ( devNum != 0 ) {
+        term_config_device = devNum;
+      } 
+      break;
+  }
+  terminal_menu();
+}
+
+void terminal_process_cmd(byte cmd, uint16_t parameter) {
+  byte devNum;
+  if (term_config_device > 0) {
+    terminal_process_device_config_cmd (cmd, parameter);
+    return;
+  }
   switch(cmd) {
     case 'f':
       devNum = terminal_verify_device(parameter, true) ;
@@ -344,6 +419,9 @@ void terminal_process_cmd(byte cmd, byte parameter) {
     case 'v':
       terminal_voltage();
       break;
+    case 'd':
+      terminal_device_config();
+      break;
     case 'x':
       terminal_disconnect();
     default:
@@ -357,7 +435,7 @@ void tty1_process_data() {
   char txBuffer[16];
   switch (rxBuffer[1]) {
     case 'v':
-      sprintf(txBuffer, "%c%d.%d%c%c", STX, (int) batteryV, (int) (batteryV *100) % 100, ETX, EOT);
+      sprintf(txBuffer, "%c%d%c%c", STX, batV, ETX, EOT);
       break;
     default:
       sprintf(txBuffer, "%c", EOT);
@@ -371,7 +449,7 @@ void tty1_process_data() {
 }
 
 int terminal0_rx() {
-  int i;
+  int i,x,m;
   // clear RX buffer
   for (i=0; i<RX_BUF_SIZE; i++) {
     rxBuffer[i] = 0;
@@ -388,7 +466,7 @@ int terminal0_rx() {
 
 int terminal1_rx() {
   // receive from uart until timeout
-  int i;
+  int i, x, m;
   // clear RX buffer
   for (i=0; i<RX_BUF_SIZE; i++) {
     rxBuffer[i] = 0;
@@ -399,6 +477,14 @@ int terminal1_rx() {
     if (tty1.available()) {
       rxBuffer[i++] = tty1.read();
       if (i >= RX_BUF_SIZE) i--;  // prevent buffer overrun      
+    }
+    // have we receive a parameter length > 1 byte?
+    if (i > 2) {  // yes - convert to integer;
+      x=i-1; m=1; rx_parameter = rxBuffer[x]-48;
+      while(--x > 0) {
+        m*=10;
+        rx_parameter += (rxBuffer[x]-48) * m;
+      }
     }
   }
   return i;
@@ -413,22 +499,33 @@ bool terminal_login() {
 
 void terminal_disconnect() {
   if (tty0_connected) {
-    terminal_print("tty0 disconnected\n\r");
+    terminal_print(F("tty0 disconnected\n\r"));
     tty0_authorized = false;
     tty0_connected = false;
   }
   if (tty1_connected) {
-    terminal_print("tty1 disconnected\n\r");
+    terminal_print(F("tty1 disconnected\n\r"));
     tty1_connected = false;
   }
+  term_config_device = 0;
 }
 
-void terminal_print(char *sFmt, ...) {
+/*
+ * print to terminal
+ * sFmt:    string stored in Flash Memory using the "F()" macro
+ * storing the string in Flash memory leaves more memory for heap (local data) and variables
+ */
+void terminal_print( const __FlashStringHelper *sFmt, ...) {
+  char sFormat[256];     // to hold format string from FLASH memory
   char acTmp[128];       // place holder for sprintf output
-  va_list args;          // args variable to hold the list of parameters
-  va_start(args, sFmt);  // mandatory call to initilase args 
 
-  vsprintf(acTmp, sFmt, args);
+  // move string from FLASH to SRAM (local memory)
+  strcpy_PF(sFormat, sFmt);
+  
+  va_list args;          // args variable to hold the list of parameters
+  va_start(args, sFormat);  // mandatory call to initilase args 
+
+  vsprintf(acTmp, sFormat, args);
   if (tty0_connected) {
     tty0.print(acTmp);
   }
@@ -455,19 +552,20 @@ void terminal_loop () {
           tty0_authorized = true;
           terminal_menu();
         } else {
-          terminal_print(" failed.\n\r");
+          terminal_print(F(" failed.\n\r"));
           terminal_disconnect();
         }
       }      
     } else {  // not connected
       if ( (rxBuffer[0] == 0x0A) || (rxBuffer[0] == 0x0D) ){
         if (tty1_connected) {
-          tty0.print("\n\rtty1 is busy - unable to connect on tty0\n\r");
-          terminal_print("\nlogin attempt on tty0\n");
+          tty0.print(F("\n\rtty1 is busy - unable to connect on tty0\n\r"));
+          terminal_print(F("\nlogin attempt on tty0\n"));
         } else {
           tty0_connected = true;
-          tty0_authorized = false;
-          terminal_print("\n\rVK2RAY\n\rconnected tty0, timeout %ds\n\rlogin:", TERMINAL_TIMEOUT);
+          tty0_authorized = true;   /// !! change to false
+          terminal_print(F("\n\rVK2RAY\n\rconnected tty0, timeout %ds\n\rlogin:"), TERMINAL_TIMEOUT);
+          tty0.println("connected");
         }
       }      
     }
@@ -482,11 +580,11 @@ void terminal_loop () {
     } else {  // not connected
       if ( (rxBuffer[0] == 0x0A) || (rxBuffer[0] == 0x0D) ) {
         if (tty0_connected) {
-          tty0.print("\n\rtty0 is busy - unable to connect on tty1\n\r");
-          terminal_print("\n\rlogin attempt on tty1\n\r");
+          tty0.print(F("\n\rtty0 is busy - unable to connect on tty1\n\r"));
+          terminal_print(F("\n\rlogin attempt on tty1\n\r"));
         } else {
           tty1_connected = true;
-          terminal_print("\n\rVK2RAY\n\rconnected tty1, timeout %ds\n\r", TERMINAL_TIMEOUT);
+          terminal_print(F("\n\rVK2RAY\n\rconnected tty1, timeout %ds\n\r"), TERMINAL_TIMEOUT);
           terminal_menu();
         }
       }
@@ -500,12 +598,13 @@ void terminal_loop () {
 
   // check timeout
   if ( (tty0_connected) && (millis() > tty0_disconnect_time) ) {
-    terminal_print("\ntimeout ..... ");
+    terminal_print(F("\ntimeout ..... "));
     terminal_disconnect();
   }
   if ( (tty1_connected) && (millis() > tty1_disconnect_time) ) {
-    terminal_print("\ntimeout ..... ");
+    terminal_print(F("\ntimeout ..... "));
     terminal_disconnect();
   }
 }
+
 
